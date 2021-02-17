5222c85d94012ceab5c2ec0e5b42ba97b816f12d
yamini <vundelayamini@gmail.com>
2021-02-17 19:21:27 +0530

    Added : example for magicNum

--- magicNum.sh
+++ magicNum.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -x
- x=25
-while [ $x !=25 ]
+
+echo "Enter the magic number between 1-100 "num
+num=1-100
+
+for ((counter=1; counter=n/2; counter++ ))
 do
-      echo $x
-       ((x++))
+      echo $counter
 done
    echo "is a magic number"
 echo "it is not a magic number"
--- tableOf2.sh
+++ tableOf2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 i=2
 
-for ((counter=1; counter<=10; counter++ ))
+for ((counter=1; counter<=128; counter++ ))
 do
       result=$(( $i * $counter ))
         echo "$i * $counter =$result"
